5970301e01b1c3b4209f94814c25de0574f82fef
CarolinaCedro <yotechgirl@gmail.com>
2022-07-10 19:22:54 -0300

    feat: up last exercise

--- Banco/src/aplication/Main.java
+++ Banco/src/aplication/Main.java
@@ -1,5 +1,7 @@
 package aplication;
 
+import entities.Transactions;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,30 +9,44 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        Transactions transactions = new Transactions();
+
 
         System.out.println("------- BANK -------");
-        System.out.println("Enter account number:: ");
-        //ler
+        System.out.println("Enter account number: ");
+        int id = sc.nextInt();
+        sc.nextLine();
         System.out.println("Enter account holder:: ");
-        //ler
+        String Name = sc.nextLine();
         System.out.println("Is there na initial deposit (y/n)?");
-        //ler
-        System.out.println("Enter initial deposit value: ");
-        //ler
+        char op = sc.next().charAt(0);
+        switch (op) {
+            case 'y':
+                System.out.println("Enter initial deposit value: ");
+                double initialValue = sc.nextDouble();
+                transactions = new Transactions(id, Name, initialValue);
+                break;
+            case 'n':
+                System.out.println("Completly");
+                transactions = new Transactions(id, Name);
+                break;
+        }
+
 
         System.out.println("Account data:");
-        //função que traz todos os dados
+        System.out.printf("Account %d, Holder: %s, Balance: $ %.2f%n", transactions.getId(), transactions.getName(), transactions.getInitialDeposit());
 
         System.out.println("Enter a deposit value:");
-        //ler
-
+        double deposit = sc.nextDouble();
+        transactions.amountAdd(deposit);
         System.out.println("Updated account data:");
-        //funcao que tráz todos os dados
-
+        transactions.report();
         System.out.println("Enter a withdraw value:");
-        //ler
-        //funcao que trás todos os dados
-
+        double witdraw = sc.nextDouble();
+        System.out.println(transactions.getAmount());
+        transactions.amountRemove(witdraw);
+        transactions.report();
+        System.out.println(transactions.getAmount());
 
 
     }
--- Banco/src/entities/Transactions.java
+++ Banco/src/entities/Transactions.java
@@ -2,13 +2,61 @@ package entities;
 
 public class Transactions {
 
-    private String Name;
+
     private int id;
+    private String Name;
     private double initialDeposit;
+    private double amount;
 
+    public Transactions() {
+    }
 
+    public Transactions(int id, String Name, double initialDeposit) {
+        this.id = id;
+        this.Name = Name;
+        this.initialDeposit = initialDeposit;
+    }
 
+    public Transactions(int id, String Name) {
+        this.id = id;
+        this.Name = Name;
 
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getInitialDeposit() {
+        return initialDeposit;
+    }
+
+    public void report() {
+        System.out.printf("Account %d, Holder: %s, Balance: $ %.2f%n", id, Name, amount);
+    }
+
+    public double amountAdd(double valueAmount) {
+        double test = initialDeposit + valueAmount;
+        this.amount = test;
+        return test;
+    }
+
+    public double amountRemove(double valueAmount){
+        double test = amount - valueAmount;
+        return test;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
 
 
 }
