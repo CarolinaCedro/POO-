5a04d7d23e926040b1054d61288af960350e450c
CarolinaCedro <yotechgirl@gmail.com>
2022-07-18 09:01:36 -0300

    feat: valueof for print

--- Enum/src/aplication/Main.java
+++ Enum/src/aplication/Main.java
@@ -1,8 +1,21 @@
 package aplication;
 
+import entities.Order;
+import entities.OrderStatus;
+
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Teste");
+        Order order = new Order(1,new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERD;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERD");
+
+
+        System.out.println(os1);
+        System.out.println(os2);
     }
 }
